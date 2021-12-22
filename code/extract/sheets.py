bc14959e1fbb38d0d4b343b91336f3a4eb7d9a62
Hrittik <67012359+hrittikhere@users.noreply.github.com>
2021-12-22 18:03:36 +0000

    Add List Looper

--- code/extract/sheets.py
+++ code/extract/sheets.py
@@ -1,9 +1,8 @@
 # This code fetches data from RSS Store on Google Sheet
-
+# export GOOGLE_APPLICATION_CREDENTIALS="keys.json"
 
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
-
 
 
 SERVICE_ACCOUNT_FILE = 'keys.json'
@@ -12,19 +11,21 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 CREDS = None
 credentials = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
 
 # The ID and range of a sample spreadsheet.
 SPREADSHEET_ID = ''
-SPREADSHEET_RANGE = 'feed!A1:G2'
+SPREADSHEET_RANGE = 'feed!C1:C113'
 service = build('sheets', 'v4', credentials=CREDS)
 
 # Call the Sheets API
 sheet = service.spreadsheets()
 result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
                             range=SPREADSHEET_RANGE).execute()
-values = result.get('values', []) # List of List
+values = result.get('values', [])  # List of List
 
-print(values) 
+
+for x in values:
+        resource = x[1]
+        print(resource)
