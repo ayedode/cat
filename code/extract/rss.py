c58893a26691b3f26e10a10bb3dc8a53bf679798
Hrittik <67012359+hrittikhere@users.noreply.github.com>
2021-12-24 16:06:11 +0000

    Add Parameters to RSS

--- code/extract/rss.py
+++ code/extract/rss.py
@@ -1,8 +1,17 @@
 from sheets import *
+from db import *
 import feedparser
 counter = 0
 print(RSS[1])
-NewsFeed = feedparser.parse(RSS[1])
-for i in range(len(NewsFeed.entries)):
-    print(counter, "    ",NewsFeed.entries[i].title, " : " , NewsFeed.entries[i].link)
-    counter += 1
+for x in RSS:
+    NewsFeed = feedparser.parse(x)
+    for i in range(len(NewsFeed.entries)):
+        print(counter, "  :  ",NewsFeed.entries[i].title, " : " , NewsFeed.entries[i].link, " : " , NewsFeed.entries[i].author , " : " , NewsFeed.entries[i].published, )
+        counter += 1
+
+
+
+
+
+# print(NewsFeed.entries[1].summary)  Find How to get the summary without the html tags
+# https://stackoverflow.com/questions/27676982/delete-html-tags-from-string-python
