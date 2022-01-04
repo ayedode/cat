9eb5857e4eccdb7bfa664ea31334f048344a4be9
Hrittik <67012359+hrittikhere@users.noreply.github.com>
2022-01-04 07:59:06 +0000

    Add TItle Exsistence Check

--- code/extract/db.py
+++ code/extract/db.py
@@ -19,14 +19,25 @@ def connect():  # Connecting to the database using the psycopg2 module
 
 
 
-def read(conn):  # Reading data from the table using the psycopg2 module
+def read_all(conn):  # Reading data from the table using the psycopg2 module
     cur = conn.cursor()
-    cur.execute("SELECT * FROM posts")
+    cur.execute("SELECT * FROM feed")
     rows = cur.fetchall()
     for row in rows:
         print("Data row = (%s, %s)" %
               (str(row[0]), str(row[1])))
     return rows
+
+
+# def checkExsistence(Title, conn):
+#     cur = conn.cursor()
+#     print(Title)
+#     cur.execute("SELECT * FROM feed WHERE TITLES=(%s);", (Title))
+#     rows = cur.fetchall()
+#     if len(rows) == 0:
+#         return False
+#     else:
+#         return True
 
 
 def write(Title, Link, conn):  # Inserting data into the table using the psycopg2 module
@@ -39,7 +50,7 @@ def write(Title, Link, conn):  # Inserting data into the table using the psycopg
 def create():  # Creating a table using the psycopg2 module
     conn = connect()
     cur = conn.cursor()
-    cur.execute("CREATE TABLE feed (TITLES VARCHAR(255), URL TEXT, AUTHOR VARCHAR(255), POSTING_DATE DATE );")
+    cur.execute("CREATE TABLE feed (TITLES VARCHAR(255) , URL TEXT, AUTHOR VARCHAR(255));")
     conn.commit()
     conn.close()
 
--- code/extract/rss.py
+++ code/extract/rss.py
@@ -1,6 +1,4 @@
 import sheets
-import time
-
 import db
 import feedparser
 
@@ -9,39 +7,55 @@ RSS = sheets.main()  # Get a List of RSS feed from Google Sheets
 conn = db.connect()
 cur = conn.cursor()
 
-# for x in RSS:
-#     NewsFeed = feedparser.parse(x)
-#     for posts in range(len(NewsFeed.entries)):
+def checkExsistence(Title):
+    cur.execute("SELECT * FROM feed WHERE TITLES=%s;", (Title,))
+    rows = cur.fetchall()
+    if len(rows) == 0:
+        return False
+    else:
+        return True
 
 
+for x in RSS:
+    NewsFeed = feedparser.parse(x)
+    for posts in range(len(NewsFeed.entries)):
 
-#         try:
-#             Author= NewsFeed.entries[posts].author
-#         except:
-#             Author="None"
+        try:
+            Author = NewsFeed.entries[posts].author
+        except:
+            Author = "None"
 
-#         try:
-#             Temp=str(NewsFeed.entries[posts].published_parsed.tm_year)+str(NewsFeed.entries[posts].published_parsed.tm_mon)+str(NewsFeed.entries[posts].published_parsed.tm_mday)
-#         except:
-#             Published=00000000
+        try:
+            Temp = str(NewsFeed.entries[posts].published_parsed.tm_year)+"-"+str(
+                NewsFeed.entries[posts].published_parsed.tm_mon)+"-"+str(NewsFeed.entries[posts].published_parsed.tm_mday)
+            Published = Temp
+        except:
+            Published = str("01-01-1999")
 
-#         try:
-#             Link=NewsFeed.entries[posts].link
-#         except:
-#             Link="Not Available"
+        try:
+            Link = NewsFeed.entries[posts].link
+        except:
+            Link = "Not Available"
 
-#         try:
-#             Title=NewsFeed.entries[posts].title
-#         except:
-#             Title="Not Available"
+        try:
+            Title = str(NewsFeed.entries[posts].title)
+        except:
+            Title = "Not Available"
 
+        if checkExsistence(Title):
+            pass
+        else:
+            cur.execute(
+                'INSERT INTO feed (Titles, URL, Author) VALUES (%s, %s, %s);', (Title, Link, Author))
 
-#         print("Now starting DB Connection", time.time())
-#         cur.execute(
-#         "INSERT INTO feed (Titles, URL, Author, POSTING_DATE) VALUES (%s, %s, %s, %s);", (Title, Link, Author, to_date()))
+        # cur.execute(
+        # "INSERT INTO feed (date) VALUES (%s);", (Published))
 
+conn.commit()
+conn.close()
 
-
-# conn.commit()
-# conn.close()
-
+# c=1
+# NewsFeed = feedparser.parse('https://www.p3r.one/feed/')
+# for posts in range(len(NewsFeed.entries)):
+#     print(c, "      ", NewsFeed.entries[posts].title)
+#     c=c+1
