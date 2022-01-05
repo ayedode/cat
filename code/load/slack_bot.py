18647c1312ac2364e175907f17ce72c71bd11479
Hrittik <67012359+hrittikhere@users.noreply.github.com>
2022-01-05 09:35:03 +0000

    Add DB read and Post to Slack

--- code/load/slack_bot.py
+++ code/load/slack_bot.py
@@ -1,10 +1,36 @@
 import slack
 from decouple import config
+import psycopg2
+from loguru import logger
 
-
+DB_NAME = config('DB_NAME')
+DB_USER = config('DB_USER')
+DB_HOST = config('DB_HOST')
+DB_PASSWORD = config('DB_PASSWORD')
 SLACK_TOKEN = config('SLACK_TOKEN')
+
+
+def connect():  
+    try:
+        conn = psycopg2.connect(
+            dbname=DB_NAME, user=DB_USER, host=DB_HOST, password=DB_PASSWORD)
+        return conn
+    except:
+        logger.debug("Unable to connect to the database")
+
+
+conn = connect()
+cur = conn.cursor()
 client = slack.WebClient(token=SLACK_TOKEN)
 
 
+cur.execute("SELECT * FROM feed")
+rows = cur.fetchall()
+# for row in rows:
+row = rows[14]
+client.chat_postMessage(
+    channel='#a-better-yt',
+    text="New Post: `"+row[1]+"` \n Visit here: "+row[2]
+)
 
-client.chat_postMessage(channel='#a-better-yt', text='Hello world!')
+logger.debug("Sent"+row[1])
