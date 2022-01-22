2b18f1362d912e22ec7a648246670d30d0fcbc88
Hrittik <67012359+hrittikhere@users.noreply.github.com>
2022-01-22 17:43:48 +0000

    Update MetaImage

--- code/extract/meta_image.py
+++ code/extract/meta_image.py
@@ -7,14 +7,10 @@ def get_image(url):
     response = urllib.request.urlopen(url)
     ParsedPage = BeautifulSoup(response, 
                          'html.parser', 
-                         from_encoding=response.info().get_param('charset'), headers={'User-Agent': 'Mozilla/5.0'})
+                         from_encoding=response.info().get_param('charset'))
 
     if ParsedPage.findAll("meta", property="og:image"):
         return ParsedPage.find("meta", property="og:image")["content"]
     else:
         return ("https://raw.githubusercontent.com/ayedode/cat/main/assests/no_image.png")
-
-
-get_image("https://devops.com/2022-will-be-the-year-of-the-cyber-shift-show/")
-
 
